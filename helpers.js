d6ca69b2d75f357f2c26257203c5ab253448f1f2
Joshua Gagnon <josh.n.gagnon@gmail.com>
2016-01-11 17:03:41 +1300

    first template done, now to do styling

---
+++ helpers.js
@@ -0,0 +1,64 @@
+import Handlebars from 'handlebars';
+import moment from 'moment';
+
+Handlebars.registerHelper('todaysDate', () => {
+    return moment().format("Do MMMM YYYY")
+});
+
+
+Handlebars.registerHelper('estimatePrice', (type) => {
+    return {
+        'purchase': '$880',
+        'sale': '$780',
+        'refinance': '$680'
+    }[type]
+});
+
+
+Handlebars.registerHelper('matterTypeString', (type) => {
+    return {
+        'purchase': 'purchase',
+        'sale': 'sale',
+        'refinance': 'refinance'
+    }[type];
+});
+
+Handlebars.registerHelper('capitalize', (string) => {
+    return string.replace(/[a-z]/, c => c.toUpperCase());
+});
+
+Handlebars.registerHelper('multiple', (items, options) => {
+    if(items.length !== 1) {
+        return options.fn(this);
+    } else {
+        return options.inverse(this);
+    }
+});
+
+Handlebars.registerHelper('joinList', (items, options) => {
+    if(items.length === 1){
+        return items[0][options.hash.prop]
+    }
+    else{
+        items = items.map(i => i[options.hash.prop]);
+        return `${items.slice(0, items.length-1).join(', ')} and ${items[items.length-1]}`
+    }
+});
+
+Handlebars.registerHelper('secondPersonPronoun', (items, options) => {
+    if(options.data.root.isCompany){
+        return options.data.root.companyNameShort;
+    }
+    else{
+        return 'you';
+    }
+});
+
+Handlebars.registerHelper('secondPersonAdjective', (items, options) => {
+    if(options.data.root.isCompany){
+        return `${options.data.root.companyNameShort}'s`;
+    }
+    else{
+        return 'your';
+    }
+});
--- server.js
+++ server.js
@@ -1,10 +1,56 @@
 import express from 'express';
+import Handlebars from 'handlebars';
+import 'fs';
+import Promise from 'bluebird';
+import _fs from 'fs';
+import './helpers';
+const fs = Promise.promisifyAll(_fs);
+
+
 const app = express();
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
 
-app.listen(3000, function() {
-  console.log('Example app listening on port 3000!');
-})
+
+let base;
+
+
+app.get('/', function(req, res) {
+    fs.readFileAsync('templates/Letter\ of\ Engagement.html', 'utf-8')
+        .then(text => {
+            const template = Handlebars.compile(text);
+            const data = {
+                individuals: [{
+                    firstName: 'Matt', lastName: 'Heath'
+                },{
+                    firstName: 'Pizza', lastName: 'Face'
+                },{
+                    firstName: 'Jimmy', lastName: 'Two-stiches'
+                }],
+                address: {
+                    street: '10 Town St',
+                    suburb: 'Suburbville',
+                    postcode: '2134',
+                    city: 'Citytopia',
+                },
+                email: 'test@example.com',
+                matterType: 'purchase',
+                properties: [{address: '1 Chingford Pl'}, {address: '2 Mingmon St'}],
+                contact: {
+                    fullName: 'Jim John',
+                    phone: '+65 234 23554',
+                    email: 'taco@time.com'
+                }
+            };
+            res.send(base({content: template(data)}))
+
+        })
+});
+app.use(express.static('assets'));
+
+fs.readFileAsync('templates/base.html', 'utf-8')
+    .then(text => { base = Handlebars.compile(text) })
+    .then(() => {
+        app.listen(3000, function() {
+          console.log('App listening on port 3000!');
+        })
+    });
