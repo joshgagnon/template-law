e597fa77c888c7febc3196b182f1aeecb205d38a
Joshua Gagnon <josh.n.gagnon@gmail.com>
2016-02-05 16:30:07 +1300

    fixed purchase

--- templates/calculations/CV10.js
+++ templates/calculations/CV10.js
@@ -4,11 +4,13 @@ export default function calculate(values){
     let credits = 0, debits = 0;
     if(values.matter){
         if(values.matter.matterType === 'purchase'){
-            credits += values.matter.loanAdvance.credit || 0;
-            credits += (values.matters.clients || []).reduce((acc, client) => {
-                return acc + (client.kiwiSaverWithdrawal.credit || 0) + (kiwiSaverHomeStart.credit || 0)
+            if(values.matter.loanAdvance) credits += values.matter.loanAdvance.credit || 0;
+            credits += (values.matter.clients || []).reduce((acc, client) => {
+                if(client.kiwiSaverWithdrawal) acc += (client.kiwiSaverWithdrawal.credit || 0);
+                if(client.kiwiSaverHomeStart) acc += (client.kiwiSaverHomeStart.credit || 0);
+                return acc;
             }, 0);
-            debits += values.balancePurchasePrice.debits || 0;
+            if(values.matter.balancePurchasePrice) debits += values.matter.balancePurchasePrice.debits || 0;
         }
         else if(values.matter.matterType === 'sale'){
             if(values.matter.balancePurchasePrice) credits += values.matter.balancePurchasePrice.credit || 0;
@@ -16,7 +18,7 @@ export default function calculate(values){
             if(values.repaymentOfIndebtedness) debits += values.matter.repaymentOfIndebtedness.debit || 0;
         }
         else{
-            credits += values.matter.loanAdvance.credit || 0;
+            if(values.matter.loanAdvance) credits += values.matter.loanAdvance.credit || 0;
             if(values.matter.repaymentOfIndebtedness) debits += values.matter.repaymentOfIndebtedness.debit || 0;
         }
         if(values.paidByTrust && values.paidByTrust.paidByTrust == "Yes"){
