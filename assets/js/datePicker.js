5c2e678bdc99b73b7be74146319cefe7e487ea65
Joshua Gagnon <josh.n.gagnon@gmail.com>
2016-02-08 10:22:36 +1300

    can now type in a few different date formats

--- assets/js/datePicker.js
+++ assets/js/datePicker.js
@@ -9,14 +9,16 @@ export default class DateInput extends React.Component {
 
     render() {
         const format="D MMMM YYYY";
-         return <DateTimePicker
+        const readFormats = [format, "D M YYYY", "D/M/YYYY", "D-M-YYYY"]
+        return <DateTimePicker
             {...this.props}
             time={false}
             value={this.props.value ? new Date(this.props.value): null }
             onChange={(date, string) => this.props.onChange(string)}
             parse={(string) => {
-                const valid = moment(string, format).isValid()
-                return valid ? moment(string, format) : null}
+                const mo = moment(string, readFormats)
+                return mo.isValid() ? mo.toDate() : null;
+                }
             }
             format={format} />
     }
