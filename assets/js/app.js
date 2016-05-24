fcbfa6f4020bfdfa3de3d45e700978701ec98178
Joshua Gagnon <josh.n.gagnon@gmail.com>
2016-05-24 12:10:08 +1200

    clean up, added error msg

--- assets/js/app.js
+++ assets/js/app.js
@@ -100,181 +100,6 @@ function applyAliases(values, aliases={}){
 }
 
 
-class AppOLDDELETE extends React.Component {
-
-    update(data) {
-        this.props.dispatch(updateValues(data));
-    }
-
-    changePageView(e) {
-        this.props.dispatch(setPageView(findDOMNode(this.refs.pageView).value));
-    }
-
-    changeFormView() {
-        this.props.dispatch(setFormView(findDOMNode(this.refs.formView).value))
-    }
-
-    reset(e) {
-        e.preventDefault();
-        const defaults = {...DEFAULT_DATA.active.values, ...(FORMS[this.props.active.form].schema.defaults || {})};
-        this.props.dispatch(updateValues({values: defaults, output: defaults}));
-    }
-
-    save(e) {
-        e.preventDefault();
-        this.props.dispatch(openModal('save'))
-    }
-
-    load(e) {
-        e.preventDefault();
-        this.props.dispatch(openModal('load'));
-    }
-
-    generate(e) {
-        e.preventDefault();
-        let filename;
-        this.props.dispatch(renderDocument({formName: this.props.active.form,
-                values: {...applyAliases(this.props.active.output, FORMS[this.props.active.form].schema.aliases),
-                    mappings: FORMS[this.props.active.form].schema.mappings }}))
-            .then((response) => {
-                if(response.error){
-                    throw response.error;
-                }
-                const disposition = response.response.headers.get('Content-Disposition')
-                filename = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/.exec(disposition)[1].replace(/"/g, '');
-                return response.response.blob()
-            })
-            .then(blob => {
-                saveAs(blob, filename);
-            })
-            .catch(() => {});
-    }
-
-    generatePreview(e) {
-        e && e.preventDefault();
-        if(this.props.preview.fetching || this.props.preview.error || this.props.preview.current || !this.valid()){
-            return;
-        }
-        this.props.dispatch(renderPreview({formName: this.props.active.form,
-                values: {...applyAliases(this.props.active.output, FORMS[this.props.active.form].schema.aliases),
-                fileType: 'pdf', mappings: FORMS[this.props.active.form].schema.mappings }}))
-            .then((response) => {
-                return response.response.blob()
-            })
-            .then(blob => {
-
-                const fileReader = new FileReader();
-                fileReader.onload = () => {
-                    this.props.dispatch(setPreview(fileReader.result));
-                };
-                fileReader.readAsArrayBuffer(blob);
-            })
-            .catch(() => {});
-    }
-
-    valid() {
-        return !Object.keys(this.props.active.errors).length
-    }
-
-    componentDidMount() {
-        this.eagerPreview();
-        this.props.dispatch(setForm({form: this.props.active.form }));
-    }
-
-    componentDidUpdate() {
-        this.eagerPreview();
-    }
-
-    eagerPreview() {
-        if(this.props.pageView.mode === 'preview' && !this.props.preview.preview){
-            this.generatePreview()
-        }
-    }
-
-    renderPageViewSelect() {
-        return <FieldWrapper label="select" title="Page View">
-              <select ref="pageView" className="form-control" onChange={::this.changePageView} value={this.props.pageView.mode}>
-                <option key={0} value={'single'}>Single</option>
-                <option key={1} value={'columns'}>Columns</option>
-                <option key={2} value={'preview'}>Preview</option>
-              </select>
-            </FieldWrapper>
-    }
-
-    renderFormViewSelect() {
-        return <FieldWrapper label="select" title="Form View">
-              <select ref="formView" className="form-control" onChange={::this.changeFormView} value={this.props.formView.mode}>
-                <option key={0} value={'full'}>Full Form</option>
-                <option key={1} value={'selected'}>Selected Forms Only</option>
-              </select>
-            </FieldWrapper>
-    }
-
-    renderTemplateSelect() {
-        return <div className="container">
-            <SectionWrapper label="select" title="Output Templates">
-            { Object.keys(FORMS).map((key, i)=>{
-                if(!FORMS[key].schema.SUPERSET){
-                    return <div key={i} className="row"><div className="form-group ">
-                        <label className="col-sm-4 control-label text-right">{ key }</label>
-                        <div className="col-sm-1"><input  type="checkbox" /></div>
-                    </div>
-                    </div>
-                }
-            }) }
-
-        </SectionWrapper>
-        </div>
-    }
-
-    render() {
-        let classes = '';
-        if(this.props.pageView.mode === 'columns'){
-            classes += 'container columns '
-        }
-        else if(this.props.pageView.mode === 'preview'){
-            classes += 'container-fluid has-preview';
-        }
-        else{
-            classes += "container ";
-        }
-        console.log(this.props)
-        return <div className="main">
-                <div className="container">
-                    <form className="form-horizontal">
-                        <div className="row">
-                            <div className="col-md-4 col-md-offset-2">
-                                { this.renderFormViewSelect() }
-                            </div>
-                            <div className="col-md-4">
-                                { this.renderPageViewSelect() }
-                            </div>
-                        </div>
-                         <div><p>
-                            <button className="btn btn-warning" onClick={::this.reset}>New Matter</button>
-                            <button className="btn btn-info" onClick={::this.load}>Load Matter</button>
-                            <button className="btn btn-info" onClick={::this.save}>Save Matter</button>
-                            </p></div>
-                    </form>
-                </div>
-                <div className={classes}>
-                    <Controls
-                        generate={::this.generate}
-                        valid={::this.valid}
-                        active={this.props.active}
-                        update={::this.update} />
-                    { this.props.pageView.mode === 'preview' && <Preview data={this.props.preview.preview } update={ ::this.generatePreview } /> }
-            </div>
-            { this.props.status.fetching && <Fetching /> }
-            { this.props.status.error && <ErrorDialog close={() => this.props.dispatch(hideError())}  /> }
-            { FORMS[this.props.active.form].emails && <EmailView values={this.props.active.values} emails={FORMS[this.props.active.form].emails} /> }
-            <StateSaver />
-            <SaveLoadDialogs />
-            { this.renderTemplateSelect() }
-        </div>
-    }
-}
-
 
 class Header extends React.Component {
     render() {
@@ -351,7 +176,8 @@ class CreateTemplates extends React.Component {
             }
             return acc;
         }, []);
-        this.props.generate(keys);
+        if(keys.length)
+            this.props.generate(keys);
     }
 
     handlePreview(key) {
@@ -535,6 +361,7 @@ class App extends React.Component {
                     preview: this.props.preview
                 })) }
             { this.props.status.fetching && <Fetching />  }
+            { this.props.status.error && <ErrorDialog close={() => this.props.dispatch(hideError())}  /> }
         </div>
     }
 }
